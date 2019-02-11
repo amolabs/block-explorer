e32bb0efaae2d23f879d1d0fa6af78e184826486
Mookeun Ji <goofcode@gmail.com>
2019-02-11 22:39:57 +0900

    Connect websocket and subscribe new block event

--- src/App.js
+++ src/App.js
@@ -6,6 +6,38 @@ import Home from './pages/Home';
 import Blocks from './pages/Blocks';
 import Transactions from './pages/Transactions';
 
+const rpcAddr = 'ws://localhost:26657/websocket';
+const ws = new WebSocket(rpcAddr);
+
+ws.onopen = e => {
+	ws.send(
+		JSON.stringify({
+			jsonrpc: '2.0',
+			method: 'subscribe',
+			id: 0,
+			params: {
+				query: "tm.event='NewBlock'",
+			},
+		})
+	);
+};
+
+ws.onmessage = e => {
+	const message = JSON.parse(e.data);
+	if (message.id === '0#event') {
+		const result = message.result.data;
+
+		const type = result.type;
+		const block = result.block;
+		const header = result.value.block.header;
+
+		console.log(`${type}: ${header}`);
+	}
+};
+ws.onerror = e => {
+	console.log('Server error message: \n', e.data);
+};
+
 class App extends Component {
 	render() {
 		return (
